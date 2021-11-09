998d0136acc1ec51a55c5af1305f9982dbae8d0a
Timur Vorobyov <buskettim0902@gmail.com>
2021-11-09 19:00:27 +0500

    chore(BodyWrapper): adds home page route

--- src/wrappers/AppWrapper/BodyWrapper/BodyWrapper.tsx
+++ src/wrappers/AppWrapper/BodyWrapper/BodyWrapper.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { BrowserRouter, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { routes } from "src/config/routes";
+import HomePage from "src/pages/HomePage";
 
 function BodyWrapper() {
   return (
     <BrowserRouter>
-      <Switch></Switch>
+      <Switch>
+        <Route path={routes.home} exact component={HomePage} />
+      </Switch>
     </BrowserRouter>
   );
 }
